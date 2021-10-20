b8d759d141b7792acc819a5289eddb2dcfc2d674
galifornia <martinho87@gmail.com>
2021-10-20 19:07:54 +0200

    Setup routes

--- router/router.go
+++ router/router.go
@@ -1,9 +1,13 @@
 package router
 
 import (
+	noteRoutes "github.com/galifornia/go-fiber-note-taking/internal/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
 func SetupRoutes(app *fiber.App) {
-	// api := app.Group("/api/v1", logger.New())
+	api := app.Group("/api/v1", logger.New())
+
+	noteRoutes.SetupNoteRoutes(&api)
 }
