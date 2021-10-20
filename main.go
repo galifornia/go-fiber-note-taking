60921c9229ad8bfa005c7ab174ff7d38d5a3f93b
galifornia <martinho87@gmail.com>
2021-10-20 17:48:37 +0200

    Extract routes from main into Router package

--- main.go
+++ main.go
@@ -10,12 +10,5 @@ func main() {
 
 	database.ConnectDB()
 
-	app.Get("/api/v1/notes", getNotes)
-
 	app.Listen(":8400")
-}
-
-func getNotes(c *fiber.Ctx) error {
-	err := c.SendString("All good!")
-	return err
 }
---
+++ router/router.go
@@ -0,0 +1,9 @@
+package router
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func SetupRoutes(app *fiber.App) {
+	// api := app.Group("/api/v1", logger.New())
+}
